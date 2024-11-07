4c6e8e829860bcb21043500f817305718af403ad
Fernanda Bertotti Guedes <fefebertotti@gmail.com>
2024-11-07 09:43:10 -0300

    Update testbench.vhd

--- sad-v1/testbench.vhd
+++ sad-v1/testbench.vhd
@@ -1,1 +1,12 @@
---inicializando pasta
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity sadv1-tb is
+end sadv1-tb;
+
+architecture tb of sadv1-tb is
+  CONSTANT n_bits : natural := 8;
+  signal clk : std_logic := '0';
+  
+  
