dffeb90c4e7a3b9e7af95dd4af23c181a543b0ac
Fernanda Bertotti Guedes <fefebertotti@gmail.com>
2024-11-07 21:47:03 -0300

    Update golden-model.py

--- sad-v3/golden-model/golden-model.py
+++ sad-v3/golden-model/golden-model.py
@@ -1,1 +1,8 @@
-#golden model sad v3
+import random
+#gera valores
+quantidade_numeros = 50
+tamanho = 8
+with open("estimulos.dat","w") as file:
+    for _ in range(quantidade_numeros):
+        numero = ''.join(str(random.randint(0,1))for _ in range(tamanho))
+        file.write(numero + "\n")
