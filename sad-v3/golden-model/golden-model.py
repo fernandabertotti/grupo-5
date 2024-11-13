05084547804128297b5c0cd0c0bc4b55e988d93d
Fernanda Bertotti Guedes <fefebertotti@gmail.com>
2024-11-13 15:34:39 -0300

    Update golden-model.py

--- sad-v3/golden-model/golden-model.py
+++ sad-v3/golden-model/golden-model.py
@@ -9,12 +9,8 @@ tamanho = 8
 sad_value = 0
 with open("estimulos.dat", "w") as file:
     for i in range(quantidade_numeros):
-        coluna0 = ''.join(str(random.randint(0, 1)) for _ in range(1))  # enable
-        coluna1 = ''.join(str(random.randint(0, 1)) for _ in range(1))  # reset
-        coluna2 = ''.join(str(random.randint(0, 1)) for _ in range(tamanho))  # mem_A
-        coluna3 = ''.join(str(random.randint(0, 1)) for _ in range(tamanho))  # mem_B
-
-        print(f"{coluna0} {coluna1} {coluna2} {coluna3} {sad_value}")
+        
+       
         #cálculo ABS(pA - pB)
         absolute = bin(abs(int(coluna2,2)-int(coluna3,2)))[2:]
         print(f"{absolute}\n")
