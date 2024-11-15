1c5beb7dd2e4313fdbf6b47dd6d69abd57614c48
Laísa Dacroce <76539042+laisadacroce@users.noreply.github.com>
2024-11-15 19:09:18 -0300

    Update sad_operativo.vhd - Alterações com mudança do absoluteDiff

--- sad-v1/sad_operativo.vhd
+++ sad-v1/sad_operativo.vhd
@@ -34,7 +34,7 @@ ARCHITECTURE arch OF sad_operativo IS
   --SIGNALS
   SIGNAL out_acc_cont : std_logic_vector(POSITIVE(ceil(real(log(real(N)) / log(2.0)))) DOWNTO 0);
   SIGNAL out_pA, out_pB : std_logic_vector(B - 1 DOWNTO 0); --saídas de pA e pB que serão entradas de absoluteDiff
-  SIGNAL out_absDiff : std_logic_vector(B - 2 DOWNTO 0); --saída do absoluteDiff
+  SIGNAL out_absDiff : std_logic_vector(B - 1 DOWNTO 0); --saída do absoluteDiff
   SIGNAL zeros_esquerda : std_logic_vector(POSITIVE(ceil(real(log(real(N)) / log(2.0)))) DOWNTO 0);
   SIGNAL in_acc_soma, out_acc_soma : std_logic_vector(B + POSITIVE(ceil(real(log(real(N)) / log(2.0)))) - 1 DOWNTO 0); --saída do absDiff concatenada com 0s à esquerda e entrada de SAD_reg
 BEGIN
@@ -50,63 +50,62 @@ BEGIN
       acumulado => out_acc_cont
     );
  
-      REG_PA : ENTITY work.registrador(arch)
+  REG_PA : ENTITY work.registrador(arch)
+        GENERIC MAP(N => B)
+      PORT MAP(
+        clk => clk, 
+        rst => rst, 
+        carga => cpA, 
+        D => Mem_A, 
+        Q => out_pA
+      );
+
+  REG_PB : ENTITY work.registrador(arch)
           GENERIC MAP(N => B)
         PORT MAP(
           clk => clk, 
           rst => rst, 
-          carga => cpA, 
-          D => Mem_A, 
-          Q => out_pA
+          carga => cpB, 
+          D => Mem_B, 
+          Q => out_pB
         );
 
-          REG_PB : ENTITY work.registrador(arch)
-              GENERIC MAP(N => B)
+  ABS_DIFF : ENTITY work.absoluteDiff(arch)
+            GENERIC MAP(N => B)
+          PORT MAP(
+            a => resize(out_pA, B+1), 
+            b => resize(out_pB, B+1), 
+            s => out_absDiff
+          );
+
+  ACC_SOMA : ENTITY work.acumulador(arch)
+             GENERIC MAP(N => B + POSITIVE(ceil(real(log(real(N)) / log(2.0)))))
+           PORT MAP(
+             clk => clk, 
+             rst => rst, 
+             carga => csoma, 
+             sel_mux => zsoma, 
+             valor_para_somar => in_acc_soma, 
+             acumulado => out_acc_soma
+           );
+
+  SAD_REG : ENTITY work.registrador(arch)
+              GENERIC MAP(N => B + POSITIVE(ceil(real(log(real(N)) / log(2.0)))))
             PORT MAP(
               clk => clk, 
               rst => rst, 
-              carga => cpB, 
-              D => Mem_B, 
-              Q => out_pB
+              carga => csad_reg, 
+              D => out_acc_soma, 
+              Q => SAD
             );
 
-              ABS_DIFF : ENTITY work.absoluteDiff(arch)
-                  GENERIC MAP(N => B)
-                PORT MAP(
-                  a => out_pA, 
-                  b => out_pB, 
-                  s => out_absDiff
-                );
+--ACRESCENTAR ZEROS À ESQUERDA DA SAÍDA DE ABSDIFF
+in_acc_soma <= resize(out_absDiff, SAD'length); --concatenação
 
-                  ACC_SOMA : ENTITY work.acumulador(arch)
-                      GENERIC MAP(N => B + POSITIVE(ceil(real(log(real(N)) / log(2.0)))))
-                    PORT MAP(
-                      clk => clk, 
-                      rst => rst, 
-                      carga => csoma, 
-                      sel_mux => zsoma, 
-                      valor_para_somar => in_acc_soma, 
-                      acumulado => out_acc_soma
-                    );
+--OBTER SAÍDA DE CONTROLE MENOR
+menor <= NOT (out_acc_cont(POSITIVE(ceil(real(log(real(N)) / log(2.0)))))); --MSB da saída do acumulador cont
 
-                      SAD_REG : ENTITY work.registrador(arch)
-                          GENERIC MAP(N => B + POSITIVE(ceil(real(log(real(N)) / log(2.0)))))
-                        PORT MAP(
-                          clk => clk, 
-                          rst => rst, 
-                          carga => csad_reg, 
-                          D => out_acc_soma, 
-                          Q => SAD
-                        );
-
-                          --ACRESCENTAR ZEROS À ESQUERDA DA SAÍDA DE ABSDIFF
-                          zeros_esquerda <= (OTHERS => '0');
-                          in_acc_soma <= zeros_esquerda & out_absDiff; --concatenação
-
-                          --OBTER SAÍDA DE CONTROLE MENOR
-                          menor <= NOT (out_acc_cont(POSITIVE(ceil(real(log(real(N)) / log(2.0)))))); --MSB da saída do acumulador cont
-
-                          --OBTER SAÍDA DE DADO ADDRESS
-                          address <= out_acc_cont(POSITIVE(ceil(real(log(real(N)) / log(2.0)))) - 1 DOWNTO 0); --bits menos significativos da saída do acumulador cont
+--OBTER SAÍDA DE DADO ADDRESS
+address <= out_acc_cont(POSITIVE(ceil(real(log(real(N)) / log(2.0)))) - 1 DOWNTO 0); --bits menos significativos da saída do acumulador cont
 
 END arch;
