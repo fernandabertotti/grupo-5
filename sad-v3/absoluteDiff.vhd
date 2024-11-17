90445486e7824c94cb883faffee16580ae9fe851
Laísa Dacroce <76539042+laisadacroce@users.noreply.github.com>
2024-11-17 20:44:13 -0300

    Update absoluteDiff.vhd

--- sad-v3/absoluteDiff.vhd
+++ sad-v3/absoluteDiff.vhd
@@ -1,46 +1,20 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-ENTITY absoluteDiff IS
-    GENERIC (N : POSITIVE);
-    PORT (
-        a : IN std_logic_vector(N - 1 DOWNTO 0);
-        b : IN std_logic_vector(N - 1 DOWNTO 0);
-        s : OUT std_logic_vector(N - 2 DOWNTO 0)
-    );
-END absoluteDiff;
+entity absoluteDiff is
+    generic (N: positive);
+    port (
+      a: IN std_logic_vector(N-1 DOWNTO 0);
+      b: in std_logic_vector(N-1 downto 0);
+      s: OUT std_logic_vector(N-1 DOWNTO 0));
+end absoluteDiff;
 
-ARCHITECTURE arch OF absoluteDiff IS
-    SIGNAL a_menos_b, b_menos_a : signed(N - 1 DOWNTO 0);
-    SIGNAL saida_mux : std_logic_vector(N - 1 DOWNTO 0);
+architecture arch of absoluteDiff is
+    signal operacao: std_logic_vector(N downto 0);
 
-BEGIN
-    SUB1 : ENTITY work.subtractor_signed(arch)
-            GENERIC MAP(N => N)
-        PORT MAP(
-            A => signed(a), 
-            B => signed(b), 
-            S => a_menos_b
-        );
-
-            SUB2 : ENTITY work.subtractor_signed(arch)
-                    GENERIC MAP(N => N)
-                PORT MAP(
-                    A => signed(b), 
-                    B => signed(a), 
-                    S => b_menos_a
-                );
-
-                    MUX : ENTITY work.mux2x1(arch)
-                            GENERIC MAP(N => N)
-                        PORT MAP(
-                            F1 => std_logic_vector(a_menos_b), 
-                            F2 => std_logic_vector(b_menos_a), 
-                            sel => a_menos_b(N - 1), 
-                            F => saida_mux
-                        );
-
-                            s <= saida_mux(N - 2 DOWNTO 0);
-
-END arch;
+begin
+    operacao <= std_logic_vector(abs(signed(resize(unsigned(a), N+1))-signed(resize(unsigned(b), N+1))));
+	 s <= operacao(N-1 downto 0);
+    
+end arch;    
