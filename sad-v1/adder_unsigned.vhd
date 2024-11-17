fa1d33b3d76a7aa4adc5e45e9d243c332112d267
Laísa Dacroce <76539042+laisadacroce@users.noreply.github.com>
2024-11-17 20:34:03 -0300

    Update adder_unsigned.vhd - Quartus

--- sad-v1/adder_unsigned.vhd
+++ sad-v1/adder_unsigned.vhd
@@ -1,18 +1,16 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
+library ieee; 
+use ieee.std_logic_1164.all; 
+use ieee.numeric_std.all;
 
-ENTITY adder_unsigned IS --é unsigned porque somente é utilizado para valores positivos (ou após o abs ou pro reg cont)
-    GENERIC (N : INTEGER);
-    PORT (
-        A, B : IN unsigned(N - 1 DOWNTO 0);
-        result : OUT std_logic_vector(N - 1 DOWNTO 0)
-    );
-END adder_unsigned;
+entity adder_unsigned is --é unsigned porque somente é utilizado para valores positivos (ou após o abs ou pro reg cont)
+generic (N : integer);
+port (A, B: in unsigned(N-1 downto 0);
+	result: out std_logic_vector(N-1 downto 0));
+end adder_unsigned;
 
-ARCHITECTURE arch OF adder_unsigned IS
-    SIGNAL sum : unsigned(N DOWNTO 0);
-BEGIN
-    sum <= resize(A, N + 1) + resize(B, N + 1);
-    result <= std_logic_vector(sum(N - 1 DOWNTO 0)); --desconsidera Cout do resultado da soma
-END arch;
+architecture arch of adder_unsigned is
+	signal sum: unsigned(N downto 0);
+begin
+	sum <= resize(A, N+1) + resize(B, N+1); 
+	result <= std_logic_vector(sum(N-1 downto 0)); --desconsidera Cout do resultado da soma
+end arch;
