1b27eaa7814abc84bac10fc00491e5a8b95df982
Laísa Dacroce <76539042+laisadacroce@users.noreply.github.com>
2024-11-20 21:58:13 -0300

    Update sad_controle.vhd - versão final

--- sad-v3/sad_controle.vhd
+++ sad-v3/sad_controle.vhd
@@ -28,11 +28,11 @@ BEGIN
                 WHEN s0 => 
                     read_mem <= '0';
                     done <= '1';
-                    zi <= '0';
+                    zi <= '-';
                     ci <= '0';
                     cpA <= '0';
                     cpB <= '0';
-                    zsoma <= '0';
+                    zsoma <= '-';
                     csoma <= '0';
                     csad_reg <= '0';
  
@@ -57,11 +57,11 @@ BEGIN
                 WHEN s2 => 
                     read_mem <= '0';
                     done <= '0';
-                    zi <= '0';
+                    zi <= '-';
                     ci <= '0';
                     cpA <= '0';
                     cpB <= '0';
-                    zsoma <= '0';
+                    zsoma <= '-';
                     csoma <= '0';
                     csad_reg <= '0';
                     IF menor = '1' THEN
@@ -73,7 +73,7 @@ BEGIN
                 WHEN s3 => 
                     read_mem <= '1';
                     done <= '0';
-                    zi <= '0';
+                    zi <= '-';
                     ci <= '0';
                     cpA <= '1';
                     cpB <= '1';
@@ -97,11 +97,11 @@ BEGIN
                 WHEN s5 => 
                     read_mem <= '0';
                     done <= '0';
-                    zi <= '0';
+                    zi <= '-';
                     ci <= '0';
                     cpA <= '0';
                     cpB <= '0';
-                    zsoma <= '0';
+                    zsoma <= '-';
                     csoma <= '0';
                     csad_reg <= '1';
                     proximo_estado <= s0;
