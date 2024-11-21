20b93e4409b5091bb164efb47042583b81de2f2f
Laísa Dacroce <76539042+laisadacroce@users.noreply.github.com>
2024-11-20 21:57:09 -0300

    Update sad_operativo.vhd - versão final

--- sad-v3/sad_operativo.vhd
+++ sad-v3/sad_operativo.vhd
@@ -37,9 +37,7 @@ ARCHITECTURE arch OF sad_operativo IS
 
     TYPE out_absdiff_array IS ARRAY (0 TO P - 1) OF std_logic_vector(B - 1 DOWNTO 0); --array para armazenar as saídas dos absoluteDiffs concatenadas com 0 à esquerda
     SIGNAL out_absdiff : out_absdiff_array; --saídas de absDiffs
-
     SIGNAL out_acc_cont : std_logic_vector(POSITIVE(ceil(log2(real(N/P)))) DOWNTO 0); --deve ter tamanho (log2 de N/P) + 1
-    SIGNAL zeros_esquerda : std_logic_vector(POSITIVE(ceil(log2(real(N)))) - 3 DOWNTO 0); --(log2 de N) -2
     SIGNAL in_add0, in_add1, in_add2, in_add3 : unsigned(B - 1 DOWNTO 0); --entradas da adderTree
     SIGNAL out_adderTree : unsigned(B + 1 DOWNTO 0); --saída de adderTree, a ser concatenada com zeros à esquerda
     SIGNAL in_acc_soma, out_acc_soma : std_logic_vector(B + POSITIVE(ceil(log2(real(N)))) - 1 DOWNTO 0); --saída de adderTree concatenada com zeros à esquerda e saída do acc soma
@@ -56,81 +54,80 @@ BEGIN
             acumulado => out_acc_cont
         );
 
-            --USO DO GENERATE PARA GERAR OS P REGISTRADORES QUE ARMAZENAM A ENTRADA MEM_A
-            gen_regA : FOR i IN 0 TO P - 1 GENERATE
-                REG_PA : ENTITY work.registrador(arch)
-                        GENERIC MAP(N => B)
-                    PORT MAP(
-                        clk => clk, 
-                        rst => rst, 
-                        carga => cpA, 
-                        D => Mem_A((i + 1) * B - 1 DOWNTO i * B), 
-                        Q => out_pA(i)
-                    );
-            END GENERATE;
+		--USO DO GENERATE PARA GERAR OS P REGISTRADORES QUE ARMAZENAM A ENTRADA MEM_A
+		gen_regA : FOR i IN 0 TO P - 1 GENERATE
+			 REG_PA : ENTITY work.registrador(arch)
+						GENERIC MAP(N => B)
+				  PORT MAP(
+						clk => clk, 
+						rst => rst, 
+						carga => cpA, 
+						D => Mem_A((i + 1) * B - 1 DOWNTO i * B), 
+						Q => out_pA(i)
+				  );
+		END GENERATE;
 
-            --USO DO GENERATE PARA CRIAR OS P REGISTRADORES QUE ARMAZENAM A ENTRADA MEM_B
-            gen_regB : FOR i IN 0 TO P - 1 GENERATE
-                REG_PB0 : ENTITY work.registrador(arch)
-                        GENERIC MAP(N => B)
-                    PORT MAP(
-                        clk => clk, 
-                        rst => rst, 
-                        carga => cpB, 
-                        D => Mem_B((i + 1) * B - 1 DOWNTO i * B), 
-                        Q => out_pB(i)
-                    );
-            END GENERATE;
+		--USO DO GENERATE PARA CRIAR OS P REGISTRADORES QUE ARMAZENAM A ENTRADA MEM_B
+		gen_regB : FOR i IN 0 TO P - 1 GENERATE
+			 REG_PB0 : ENTITY work.registrador(arch)
+						GENERIC MAP(N => B)
+				  PORT MAP(
+						clk => clk, 
+						rst => rst, 
+						carga => cpB, 
+						D => Mem_B((i + 1) * B - 1 DOWNTO i * B), 
+						Q => out_pB(i)
+				  );
+		END GENERATE;
 
-            --USO DO GENERATE PARA CRIAR OS P MÓDULOS ABSOLUTEDIFF
-            gen_abs_diff : FOR i IN 0 TO P - 1 GENERATE
-                ABS_DIFF : ENTITY work.absoluteDiff(arch)
-                        GENERIC MAP(N => B)
-                    PORT MAP(
-                        a => out_pA(i), 
-                        b => out_pB(i), 
-								s => out_absdiff(i)(B - 1 DOWNTO 0)); 
-            END GENERATE;
+		--USO DO GENERATE PARA CRIAR OS P MÓDULOS ABSOLUTEDIFF
+		gen_abs_diff : FOR i IN 0 TO P - 1 GENERATE
+			 ABS_DIFF : ENTITY work.absoluteDiff(arch)
+						GENERIC MAP(N => B)
+				  PORT MAP(
+						a => out_pA(i), 
+						b => out_pB(i), 
+						s => out_absdiff(i)); 
+		END GENERATE;
 
-            ADDERTREE : ENTITY work.adderTree(arch) --DÚVIDA: como seria possível deixar mais genérico? Para essa solução funcionar, sempre teria que ter ao menos 4 operandos
-                    GENERIC MAP(N => B)
-                PORT MAP(
-                    a => unsigned(out_absdiff(0)), 
-                    b => unsigned(out_absdiff(1)), 
-                    c => unsigned(out_absdiff(2)), 
-                    d => unsigned(out_absdiff(3)), 
-                    s => out_adderTree
-                );
+		ADDERTREE : ENTITY work.adderTree(arch) --Para essa solução funcionar, sempre teria que ter ao menos 4 operandos ou múltiplos de 4
+			 GENERIC MAP(N => B)
+			 PORT MAP(
+				  a => unsigned(out_absdiff(0)), 
+				  b => unsigned(out_absdiff(1)), 
+				  c => unsigned(out_absdiff(2)), 
+				  d => unsigned(out_absdiff(3)), 
+				  s => out_adderTree
+			 );
 
-                    ACC_SOMA : ENTITY work.acumulador(arch)
-                            GENERIC MAP(N => B + POSITIVE(ceil(log2(real(N)))))
-                        PORT MAP(
-                            clk => clk, 
-                            rst => rst, 
-                            carga => csoma, 
-                            sel_mux => zsoma, 
-                            valor_para_somar => in_acc_soma, 
-                            acumulado => out_acc_soma
-                        );
+		ACC_SOMA : ENTITY work.acumulador(arch)
+			  GENERIC MAP(N => B + POSITIVE(ceil(log2(real(N)))))
+			  PORT MAP(
+				 clk => clk, 
+				 rst => rst, 
+				 carga => csoma, 
+				 sel_mux => zsoma, 
+				 valor_para_somar => in_acc_soma, 
+				 acumulado => out_acc_soma
+			);
 
-                            SAD_REG : ENTITY work.registrador(arch)
-                                    GENERIC MAP(N => B + POSITIVE(ceil(log2(real(N)))))
-                                PORT MAP(
-                                    clk => clk, 
-                                    rst => rst, 
-                                    carga => csad_reg, 
-                                    D => out_acc_soma, 
-                                    Q => SAD
-                                );
+		SAD_REG : ENTITY work.registrador(arch)
+				GENERIC MAP(N => B + POSITIVE(ceil(log2(real(N)))))
+				PORT MAP(
+				clk => clk, 
+				rst => rst, 
+				carga => csad_reg, 
+				D => out_acc_soma, 
+				Q => SAD
+		  );
 
-                                    --ACRESCENTAR ZEROS À ESQUERDA DA SAÍDA DE ADDERTREE
-                                    zeros_esquerda <= (OTHERS => '0');
-                                    in_acc_soma <= zeros_esquerda & std_logic_vector(out_adderTree); --concatenação
+	--ACRESCENTAR ZEROS À ESQUERDA DA SAÍDA DE ADDERTREE
+	in_acc_soma <= std_logic_vector(resize(out_adderTree, SAD'length)); 
 
-                                    --OBTER SAÍDA DE CONTROLE MENOR
-                                    menor <= NOT (out_acc_cont(POSITIVE(ceil(log2(real(N/P)))))); --MSB da saída do acumulador cont
+	--OBTER SAÍDA DE CONTROLE MENOR
+	menor <= NOT (out_acc_cont(POSITIVE(ceil(log2(real(N/P)))))); --MSB da saída do acumulador cont
 
-                                    --OBTER SAÍDA DE DADO ADDRESS
-                                    address <= out_acc_cont(POSITIVE(ceil(log2(real(N/P)))) - 1 DOWNTO 0); --bits menos significativos da saída do acumulador cont
+	--OBTER SAÍDA DE DADO ADDRESS
+	address <= out_acc_cont(POSITIVE(ceil(log2(real(N/P)))) - 1 DOWNTO 0); --bits menos significativos da saída do acumulador cont
 
 END arch;
